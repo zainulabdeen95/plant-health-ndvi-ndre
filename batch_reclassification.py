68ade2e649c329e8308202fdb094d6df25d13570
Zain <zainulabdeen@farmdar.co.uk>
2024-11-19 15:12:37 +0500

    thresholding function added

--- batch_reclassification.py
+++ batch_reclassification.py
@@ -3,12 +3,18 @@ import rasterio
 import numpy as np
 
 # Defining the reclassification function
-def reclassify_raster(input_path, output_path, report):
+import numpy as np
+import rasterio
+
+def reclassify_raster(input_path, output_path, report, threshold=0.2):
     with rasterio.open(input_path) as src:
         arr = src.read(1)
         
         # Masking out zero values (interpreted as null)
         arr = np.where(arr == 0, np.nan, arr)
+        
+        # Apply the threshold: Anything lower than the threshold will have class 1
+        arr = np.where(arr < threshold, 1, arr)
         
         # Calculating mean and standard deviation, ignoring NaN values
         mean = np.nanmean(arr)
@@ -23,14 +29,12 @@ def reclassify_raster(input_path, output_path, report):
         if report == "ndvi":
 
             # Reclassifying based on thresholds for NDVI & GCI
-            arr = np.where(arr <= val1, 1, arr)
             arr = np.where((arr > val1) & (arr <= val2), 2, arr)
             arr = np.where((arr > val2) & (arr <= val3), 3, arr)
             arr = np.where((arr > val3) & (arr < 1), 4, arr)
 
         elif report == "ndre":
-        # Reclassifying based on thresholds for NDRE
-            arr = np.where(arr <= val1, 1, arr)
+            # Reclassifying based on thresholds for NDRE
             arr = np.where((arr > val1) & (arr <= val2), 2, arr)
             arr = np.where((arr > val2) & (arr < 1), 3, arr)
         
@@ -45,6 +49,7 @@ def reclassify_raster(input_path, output_path, report):
         with rasterio.open(output_path, "w", **out_meta) as dest:
             dest.write(arr.astype(rasterio.int32), 1)
     print(f"Reclassified raster saved at: {output_path}")
+
 
 
 
